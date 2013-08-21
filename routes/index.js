3735ea8c670973cb9704e69c63abdf44449f8060
superbug <sunweijun1989@gmail.com>
2013-08-21 20:14:09 +0800

    add mysql modules

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,25 @@
 /*
  * GET home page.
  */
+var mysql = require('mysql');
+var data = null;
+var connection = mysql.createConnection({
+      host: 'localhost',
+      user: 'root',
+      password: '123456',
+      database: 'test'
+});
 
+connection.connect();
+
+// connection.query('SELECT * from  wine', function(err, rows, fields) {
+//   if (err) throw err;
+//   console.log(rows);
+//   data = rows;
+// });
+
+// connection.end();
+console.log(data);
 exports.index = function(req, res){
   res.render('index', { title: 'Express' });
 };
