d765b6010f88c983f4129111fabca6b5c5785d08
superbug <sunweijun1989@gmail.com>
2013-09-01 05:49:23 +0800

    connect to mysql successfully

--- routes/index.js
+++ routes/index.js
@@ -5,22 +5,23 @@
 var mysql = require('mysql');
 var data = null;
 var connection = mysql.createConnection({
-      host: 'localhost',
-      user: 'root',
-      password: '123456',
-      database: 'test'
+        host: 'localhost',
+        user: 'root',
+        socketPath: '/var/run/mysqld/mysqld.sock',  
+        password: '123456',
+        database: 'test'
 });
 
-connection.connect();
+connection.connect(function(err) {
+    console.log(err);    
+});
 
-connection.query('SELECT * from  `ent_GuoJiaAnJianZongJu_datapool_XingZhengZhiFa`', function(err, rows, fields) {
+connection.query('SELECT * from  `wine`', function(err, rows, fields) {
   if (err) throw err;
-  console.log(rows);
   data = rows;
 });
 
 connection.end();
-console.log(data);
 exports.index = function(req, res){
   res.render('index', { title: 'Cars' });
 }; 
